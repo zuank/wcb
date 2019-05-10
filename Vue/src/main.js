1525a00cd24f8de0933bca134680b15fb7177429
Yuehao Wang <352209095@qq.com>
2019-05-10 15:28:54 +0800

    eslint

--- Vue/src/main.js
+++ Vue/src/main.js
@@ -10,14 +10,14 @@ import axios from './utils/axios'
 import './assets/css/common.css'
 
 toast.config({
-  position: 'top', // 弹出的位置
-  time: 2000, // 显示的时长
-  closeIcon: 'close', // 关闭的图标
-  close: true, // 是否显示关闭按钮
-  successIcon: 'check_circle', // 成功信息图标
-  infoIcon: 'info', // 信息信息图标
-  warningIcon: 'priority_high', // 提醒信息图标
-  errorIcon: 'warning' // 错误信息图标
+    position: 'top', // 弹出的位置
+    time: 2000, // 显示的时长
+    closeIcon: 'close', // 关闭的图标
+    close: true, // 是否显示关闭按钮
+    successIcon: 'check_circle', // 成功信息图标
+    infoIcon: 'info', // 信息信息图标
+    warningIcon: 'priority_high', // 提醒信息图标
+    errorIcon: 'warning' // 错误信息图标
 })
 
 Vue.prototype.$http = axios
@@ -29,6 +29,6 @@ sync(store, router)
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 export default new Vue({
-  router,
-  store
+    router,
+    store
 }).$mount('#app')
--- Vue/src/page/login/index.vue
+++ Vue/src/page/login/index.vue
@@ -6,93 +6,93 @@
 </style>
 
 <template>
-  <div class="login">
-    <div class="form">
-      <mu-text-field v-model="userName"
-                     label="用户名"
-                     labelFloat />
-      <br />
-      <mu-text-field v-model="password"
-                     label="密码"
-                     type="password"
-                     labelFloat />
-      <br />
-      <mu-button color="primary"
-                 @click="goLogin">登录</mu-button>
-      <mu-button color="primary"
-                 @click="goReg">注册</mu-button>
-    </div>
+    <div class="login">
+        <div class="form">
+            <mu-text-field v-model="userName"
+                           label="用户名"
+                           labelFloat />
+            <br />
+            <mu-text-field v-model="password"
+                           label="密码"
+                           type="password"
+                           labelFloat />
+            <br />
+            <mu-button color="primary"
+                       @click="goLogin">登录</mu-button>
+            <mu-button color="primary"
+                       @click="goReg">注册</mu-button>
+        </div>
 
-  </div>
+    </div>
 </template>
 
 <script>
 import { mapState } from 'vuex'
 
 export default {
-  name: 'login',
-  data() {
-    return {
-      userName: '',
-      password: ''
-    }
-  },
-  computed: {
-    ...mapState(['route', 'user'])
-  },
-  watch: {
-    user: function() {
-      this.watchUser()
-    }
-  },
-  mounted() {
-    this.watchUser()
-  },
-  methods: {
-    watchUser() {
-      if (this.user.userId) {
-        this.$router.push({
-          name: 'JourneyList'
-        })
-      }
+    name: 'login',
+    data() {
+        return {
+            userName: '',
+            password: ''
+        }
     },
-    goLogin() {
-      this.$http
-        .post('/api/login', {
-          userName: this.userName,
-          password: this.password
-        })
-        .then(
-          response => {
-            if (response.data.status === 0) {
-              // 获取用户信息
-              this.$store.dispatch('getUserInfo')
-              this.$toast.success('登录成功')
-              this.$router.push('journey-list')
-            }
-          },
-          response => {
-            // error callback
-          }
-        )
+    computed: {
+        ...mapState(['route', 'user'])
     },
-    goReg() {
-      this.$http
-        .post('/api/reg', {
-          userName: this.userName,
-          password: this.password
-        })
-        .then(
-          response => {
-            if (response.data.status === 0) {
-              this.$toast.success('注册成功')
+    watch: {
+        user: function() {
+            this.watchUser()
+        }
+    },
+    mounted() {
+        this.watchUser()
+    },
+    methods: {
+        watchUser() {
+            if (this.user.userId) {
+                this.$router.push({
+                    name: 'JourneyList'
+                })
             }
-          },
-          response => {
-            // error callback
-          }
-        )
+        },
+        goLogin() {
+            this.$http
+                .post('/api/login', {
+                    userName: this.userName,
+                    password: this.password
+                })
+                .then(
+                    response => {
+                        if (response.data.status === 0) {
+                            // 获取用户信息
+                            this.$store.dispatch('getUserInfo')
+                            this.$toast.success('登录成功')
+                            this.$router.push('journey-list')
+                        }
+                    },
+                    response => {
+                        // error callback
+                    }
+                )
+        },
+        goReg() {
+            this.$http
+                .post('/api/reg', {
+                    userName: this.userName,
+                    password: this.password
+                })
+                .then(
+                    response => {
+                        if (response.data.status === 0) {
+                            this.$toast.success('注册成功')
+                        }
+                    },
+                    response => {
+                        // error callback
+                    }
+                )
+        }
     }
-  }
 }
 </script>
